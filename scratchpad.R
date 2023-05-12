986ba7d2d69c95687b02bd0df854ce76ea30ae56
suspicious-cow <zain_naboulsi@msn.com>
2023-05-12 18:16:08 -0500

    moving the package load to the main sample creation page

--- scratchpad.R
+++ scratchpad.R
@@ -15,27 +15,7 @@ for (lib in required_libraries) {
   library(lib, character.only = TRUE)
 }
 
-# Initial load of the data
-# Define the path to our data and the files we want
-directory <- "SwiftKeyData/*.txt"  
 
-# Read the text files
-text_data <- readtext(directory)
-
-# Create a corpus to hold all our text data
-corpus <- corpus(text_data)
-
-# Next we tokenize our data to begin breaking it down
-corpus <- tokens(corpus)
-
-# Remove stop words to aren't interesting to our analysis
-corpus <- tokens_remove(corpus, stopwords("english"))
-
-# Finally, we create our document feature matrix to get a handle
-# on the token frequency
-dfm <- dfm(corpus)
-
-# some change
 
 
 
