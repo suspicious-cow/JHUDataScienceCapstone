b02cb66f71361567a2cd0ef0140d12c626c03178
suspicious-cow <zain_naboulsi@msn.com>
2023-05-12 17:18:03 -0500

    changing up how the files are saved and adding an overwrite feature to the sample file generation

--- SampleCreation.R
+++ SampleCreation.R
@@ -1,5 +1,3 @@
-
-
 sample_lines2 <- function(file, n) {
   total_lines <- sum(readLines(file) != "")
   
@@ -32,17 +30,18 @@ blogs_sample <- sample_lines2("SwiftKeyData/en_US.blogs.txt", sample_size)
 news_sample <- sample_lines2("SwiftKeyData/en_US.news.txt", sample_size)
 twitter_sample <- sample_lines2("SwiftKeyData/en_US.twitter.txt", sample_size)
 
-write.table(blogs_sample, file = "SampleData/blogs_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(news_sample, file = "SampleData/news_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(twitter_sample, file = "SampleData/twitter_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(blogs_sample, file = "SampleData/blogs_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+write.table(news_sample, file = "SampleData/news_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+write.table(twitter_sample, file = "SampleData/twitter_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
 
-data_sample <- rbind(blogs_sample, news_sample, twitter_sample)
-write.table(full_data_sample, file = "SampleData/full_data_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+full_data_sample <- rbind(blogs_sample, news_sample, twitter_sample)
+write.table(full_data_sample, file = "SampleData/full_data_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+
 
 
 
 # Create a TextDocument object
-tdm <- TextDocument(data_sample$V1)
+tdm <- TextDocument(full_data_sample$V1)
 
 # Create n-grams
 ngram <- ngram(tdm, n = 3) # Change n as per your requirement
