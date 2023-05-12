7dcfb2325f89baf2825c164fe1a91c5a7a06d8f7
suspicious-cow <zain_naboulsi@msn.com>
2023-05-12 17:26:37 -0500

    logic to load the sample files into memory instead of rewriting them everytime

--- SampleCreation.R
+++ SampleCreation.R
@@ -26,21 +26,42 @@ sample_lines2 <- function(file, n) {
 }
 
 sample_size <- 10000
-blogs_sample <- sample_lines2("SwiftKeyData/en_US.blogs.txt", sample_size)
-news_sample <- sample_lines2("SwiftKeyData/en_US.news.txt", sample_size)
-twitter_sample <- sample_lines2("SwiftKeyData/en_US.twitter.txt", sample_size)
 
-write.table(blogs_sample, file = "SampleData/blogs_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
-write.table(news_sample, file = "SampleData/news_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
-write.table(twitter_sample, file = "SampleData/twitter_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+# Check if the file exists before sampling
+if (!file.exists("SampleData/blogs_sample.txt")) {
+  blogs_sample <- sample_lines2("SwiftKeyData/en_US.blogs.txt", sample_size)
+  write.table(blogs_sample, file = "SampleData/blogs_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+} else {
+  blogs_sample <- read.table("SampleData/blogs_sample.txt", header = FALSE, stringsAsFactors = FALSE)
+}
 
-full_data_sample <- rbind(blogs_sample, news_sample, twitter_sample)
-write.table(full_data_sample, file = "SampleData/full_data_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+if (!file.exists("SampleData/news_sample.txt")) {
+  news_sample <- sample_lines2("SwiftKeyData/en_US.news.txt", sample_size)
+  write.table(news_sample, file = "SampleData/news_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+} else {
+  news_sample <- read.table("SampleData/news_sample.txt", header = FALSE, stringsAsFactors = FALSE)
+}
+
+if (!file.exists("SampleData/twitter_sample.txt")) {
+  twitter_sample <- sample_lines2("SwiftKeyData/en_US.twitter.txt", sample_size)
+  write.table(twitter_sample, file = "SampleData/twitter_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+} else {
+  twitter_sample <- read.table("SampleData/twitter_sample.txt", header = FALSE, stringsAsFactors = FALSE)
+}
+
+
+if (!file.exists("SampleData/full_data_sample.txt")) {
+  full_data_sample <- c(blogs_sample, news_sample, twitter_sample)
+  write.table(full_data_sample, file = "SampleData/full_data_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, append = FALSE)
+} else {
+  full_data_sample <- read.table("SampleData/full_data_sample.txt", header = FALSE, stringsAsFactors = FALSE)[,1]
+}
+
 
 
 
 # Convert the text to a corpus
-corp <- corpus(full_data_sample$V1)
+corp <- corpus(full_data_sample)
 
 # Convert the corpus to tokens
 tokens <- tokens(corp)
