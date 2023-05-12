1f344365b54366e6702ea66186ee3a324b83249e
suspicious-cow <zain_naboulsi@msn.com>
2023-05-12 17:13:24 -0500

    using readlines to read a line in real-time and choose them randomly

--- SampleCreation.R
+++ SampleCreation.R
@@ -1,20 +1,36 @@
 
 
-sample_lines <- function(file, n) {
+sample_lines2 <- function(file, n) {
   total_lines <- sum(readLines(file) != "")
-  line_numbers <- data.frame(line_number = 1:total_lines)
-  sample_line_numbers <- sqldf(paste0("SELECT * FROM line_numbers 
-                                        ORDER BY RANDOM() 
-                                        LIMIT ", n))
-  read.table(file, skip = setdiff(1:total_lines, sample_line_numbers$line_number),
-             header = FALSE, stringsAsFactors = FALSE)
+  
+  # Determine the probability of selection for each line
+  p <- n / total_lines
+  
+  # Initialize an empty vector to store the selected lines
+  lines_sample <- c()
+  
+  # Read the file line by line
+  con <- file(file, "r")
+  while(TRUE) {
+    line <- readLines(con, n = 1)
+    if(length(line) == 0) {
+      break
+    }
+    
+    # Add the line to the sample with probability p
+    if(runif(1) < p) {
+      lines_sample <- c(lines_sample, line)
+    }
+  }
+  close(con)
+  
+  return(lines_sample)
 }
 
-
 sample_size <- 10000
-blogs_sample <- sample_lines("SwiftKeyData/en_US.blogs.txt", sample_size)
-news_sample <- sample_lines("SwiftKeyData/en_US.news.txt", sample_size)
-twitter_sample <- sample_lines("SwiftKeyData/en_US.twitter.txt", sample_size)
+blogs_sample <- sample_lines2("SwiftKeyData/en_US.blogs.txt", sample_size)
+news_sample <- sample_lines2("SwiftKeyData/en_US.news.txt", sample_size)
+twitter_sample <- sample_lines2("SwiftKeyData/en_US.twitter.txt", sample_size)
 
 write.table(blogs_sample, file = "SampleData/blogs_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 write.table(news_sample, file = "SampleData/news_sample.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
