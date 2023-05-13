ca09d42668b8ccb037bce34db2e980ab3766a024
suspicious-cow <zain_naboulsi@msn.com>
2023-05-12 21:39:10 -0500

    Last bit before working on the milestone. Added html render for final project report as well.

--- Visualizations.R
+++ Visualizations.R
@@ -5,6 +5,45 @@ top_tokens <- topfeatures(tokens_dfm, n = 20) # change n to get more or less tok
 
 print(top_tokens)
 
+# word clouds
+# Set the base font size
+par(cex = 3)  # Increase the size by 50%
+
+# Generate the word cloud with stopwords
+textplot_wordcloud(tokens_dfm)
+
+
+# Generate the word cloud without stopwords
+textplot_wordcloud(tokens_dfm_nostop)
+
+
+# Reset the base font size to the default
+par(cex = 1)
+
+
+# Get the total frequency of each word
+freq <- colSums(as.matrix(tokens_dfm))
+
+# Sort the frequencies in decreasing order
+freq_sorted <- sort(freq, decreasing = TRUE)
+
+# Calculate the cumulative frequency
+cumulative_freq <- cumsum(freq_sorted)
+
+# Calculate the total frequency
+total_freq <- sum(freq_sorted)
+
+# Find the number of words that make up 50% and 90% of the total frequency
+num_words_50_percent <- min(which(cumulative_freq / total_freq >= 0.50))
+num_words_90_percent <- min(which(cumulative_freq / total_freq >= 0.90))
+
+print(paste("Number of unique words covering 50% of all word instances: ", num_words_50_percent))
+print(paste("Number of unique words covering 90% of all word instances: ", num_words_90_percent))
+
+
+
+# first histogram with stopwords
+
 # Extract word frequencies
 freq <- textstat_frequency(tokens_dfm)
 
